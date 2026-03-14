3056222269cb65d7d7a5c241889ebbafd7519a91
Akshay Chaudhari <your-email@example.com>
2026-03-14 09:14:39 +0530

    your commit message

--- omnix-ai/.next/dev/static/chunks/_a0ff3932._.js
+++ omnix-ai/.next/dev/static/chunks/_a0ff3932._.js
@@ -1,14 +1,14 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_956a0d3a._.js",
-  "static/chunks/node_modules_next_dist_compiled_react-dom_1e674e59._.js",
-  "static/chunks/node_modules_next_dist_compiled_react-server-dom-turbopack_9212ccad._.js",
-  "static/chunks/node_modules_next_dist_compiled_next-devtools_index_1dd7fb59.js",
-  "static/chunks/node_modules_next_dist_compiled_a0e4c7b4._.js",
-  "static/chunks/node_modules_next_dist_client_17643121._.js",
-  "static/chunks/node_modules_next_dist_f3530cac._.js",
-  "static/chunks/node_modules_@swc_helpers_cjs_d80fb378._.js"
+  "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_106881dd._.js",
+  "static/chunks/b2da0_next_dist_compiled_react-dom_04addd25._.js",
+  "static/chunks/b2da0_next_dist_compiled_react-server-dom-turbopack_5a7de1eb._.js",
+  "static/chunks/b2da0_next_dist_compiled_next-devtools_index_25f50033.js",
+  "static/chunks/b2da0_next_dist_compiled_391f9a1b._.js",
+  "static/chunks/b2da0_next_dist_client_fbb6e4d0._.js",
+  "static/chunks/b2da0_next_dist_171a6a31._.js",
+  "static/chunks/69652_@swc_helpers_cjs_679851cc._.js"
 ],
     source: "entry"
 });
